f29d3a4c0c6b8c8024fae5484bfbb54fcd7db563
sshaper <sshaper@sshaper.com>
2021-09-14 20:56:54 -0400

    updates

---
+++ html_examples/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>html example</title>
+</head>
+<body>
+
+<!-- <div></div> -->
+    <header>
+        <h1><div></div>This is a header</h1>
+
+        <nav>
+            <ul>
+                <li><a href="http://198.199.80.235/cps276/cps276_examples/html_examples/index.php">Home</a></li>
+                <li><a href="http://198.199.80.235/cps276/cps276_examples/html_examples/page1.php">Page 1</a></li>
+                <li><a href="http://198.199.80.235/cps276/cps276_examples/html_examples/subfolder/index.php">Sub page 1</a></li>
+                <li><a href="https://google.com">Google</a></li>
+            </ul>
+
+
+        </nav>
+
+    </header>
+
+    <main>
+        <ol>
+            <li>List item 1</li>
+            <li>List item 2
+                <ul>
+                    <li>sub list 1</li>
+                    <li>sub list 2</li>
+                </ul>
+
+            </li>
+
+
+            
+            <li>List item 3</li>
+
+        </ol>
+
+        <table border="1">
+        <tr>
+            <th colspan="4">Table Header</th>
+        </tr>    
+        
+        <tr>
+                <td rowspan="2">row 1 cell 1</td>
+                <td>row 1 cell 2</td>
+                <td>row 1 cell 3</td>
+                <td>row 1 cell 4</td>
+
+            </tr>
+            <tr>
+                
+                <td>row 2 cell 2</td>
+                <td>row 2 cell 3</td>
+                <td>row 2 cell 4</td>
+
+            </tr>
+
+        </table>
+
+
+
+
+        <div>
+            <pre>
+                Just a quick       example...
+                ...to show        how pre-formatted text renders
+
+
+
+
+
+
+                notice I have multiple spaces between words and lines
+            </pre>
+
+        </div>
+
+    </main>
+</body>
+</html>
---
+++ html_examples/page1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page 1</title>
+</head>
+<body>
+    <header>
+        <h1>This is page 1</h1>
+        <nav>
+        <ul>
+                <li><a href="http://198.199.80.235/cps276/cps276_examples/html_examples/index.php">Home</a></li>
+                <li><a href="http://198.199.80.235/cps276/cps276_examples/html_examples/page1.php">Page 1</a></li>
+                <li><a href="http://198.199.80.235/cps276/cps276_examples/html_examples/subfolder/index.php">Sub page 1</a></li>
+            </ul>
+
+        </nav>
+    </header>
+</body>
+</html>

25ab7a8c2f13a82e0f5f05200dc06489f87df70a
sshaper <sshaper@sshaper.com>
2023-03-27 15:30:42 -0400

    updates

--- ajax/php/sendObj.php
+++ ajax/php/sendObj.php
@@ -1,7 +1,46 @@
 <?php
 
 /* JSON_DECODE TAKES THE JSON STRING OF THE POST DATA (DATA IS THE NAME THAT CONTAINS THE STRING THAT WAS SENT) AND TURNS IT INTO A PHP OBJECT.  IN THIS CASE THAT OBJECT IS NAMED $DATA */
+
 $data = json_decode($_POST['data']);
+
+/* THIS IS WHERE YOU WOULD ADD THE INFORMATION TO AT DATABASE IF THAT IS WHAT YOU NEEDED TO DO, IT IS JUST LIKE YOU HAVE DONE BEFORE.
+EXAMPLE:
+
+$pdo = new PdoMethods();
+
+//HERE I CREATE THE SQL STATEMENT I AM BINDING THE PARAMETERS 
+$sql = "INSERT INTO names (fname, lname, address, city) VALUES (:fname, :lname, :address, :city)";
+
+		
+//THESE BINDINGS ARE LATER INJECTED INTO THE SQL STATEMENT THIS PREVENTS AGAIN SQL INJECTIONS 
+$bindings = [
+	[':fname',$data->fname,'str'],
+	[':lname',$data->lname,'str'],
+	[':address',$data->address,'str'],
+	[':city',$data->city,'str']
+];
+
+//I AM CALLING THE OTHERBINDED METHOD FROM MY PDO CLASS 
+$result = $pdo->otherBinded($sql, $bindings);
+
+//HERE I AM RETURNING EITHER AN ERROR STRING OR A SUCCESS STRING 
+if($result === 'error'){
+	$response = (object)[
+	'masterstatus'=>'error',
+	'resp'=>"There was an error entering the name
+];
+}
+else {
+	$response = (object)[
+	'masterstatus'=>'success',
+	'resp'=>"Name has been added".
+];
+}
+	
+*/
+
+
 
 /* HERE I CREATE A STRING THAT ADDS THE VALUE OF THE $DATA OBECT TO A STRING.  */
 $output = "The first name is {$data->fname}<br>";
